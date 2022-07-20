ad2113f0568de0d9de8ac9be4b68daeb56bca33b
prof.juanPablo <prof.juanPablo@gmail.com>
2022-07-20 09:57:03 -0300

    adicionado os componentes sliding e alert

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +8,38 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor(public alertController: AlertController) {}
+
+  async exibirAlertFavorito() {
+    const alert = await this.alertController.create({
+      //cssClass: 'my-custom-class',
+      header: 'Alerta',
+     // subHeader: 'Subtitle',
+      message: 'Deseja favovitar este Filme.',
+      buttons: [
+        {
+          text:'Cancelar',
+          role: 'cancel',
+          cssClass:'secondary',
+          handler: (blah)=>{
+            console.log('Confirma Cancel:bla')
+          }
+        },
+        {
+          text: 'Sim, FAvoritar',
+          handler:() => {
+            console.log('Confirm Ok')
+          }
+
+        }
+      ],
+
+    });
+
+    await alert.present();
+
+    const { role } = await alert.onDidDismiss();
+    console.log('onDidDismiss resolved with role', role);
+  }
 
 }
